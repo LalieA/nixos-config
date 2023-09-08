7be7affbc4d68fbe53dd38cb1b24796b9a115cd0
LalieA <83229713+LalieA@users.noreply.github.com>
2023-09-09 00:58:14 +0200

    firefox: Add profile settings
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -3,5 +3,27 @@
 {
     programs.firefox = {
         enable = true;
+        profiles = {
+            default = {
+                id = 0;
+                search.force = true;
+                settings = {
+                    "app.update.enabled" = false;
+                    "toolkit.telemetry.prompted" = "2";
+                    "toolkit.telemetry.rejected" = true;
+                    "browser.download.autohideButton" = false;
+                    "browser.download.panel.shown" = true;
+                    "browser.shell.checkDefaultBrowser" = false;
+                    "browser.toolbars.bookmarks.visibility" = "always";
+                    "browser.startup.homepage" = "https://www.google.fr";
+                    "browser.search.region" = "FR";
+                    "browser.search.isUS" = false;
+                    "browser.formfill.enable" = false;
+                    "privacy.clearOnShutdown.cookies" = false;
+                    "privacy.sanitize.sanitizeOnShutdown" = true;
+                    "network.cookie.thirdparty.sessionOnly" = true;
+                };
+            };
+        };
     };
 }
