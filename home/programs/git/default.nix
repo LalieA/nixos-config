9d724b404b2b66375b680b22ab20ef1bac0f33f6
LalieA <83229713+LalieA@users.noreply.github.com>
2023-09-09 00:47:24 +0200

    git: Add aliases in git configuration
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -5,5 +5,14 @@
         enable = true;
         userName = "LalieA";
         userEmail = "83229713+LalieA@users.noreply.github.com";
+
+        aliases = {
+            co = "checkout";
+            br = "branch";
+            ci = "commit";
+            st = "status";
+            rb = "rebase";
+            log = "log --all --graph --oneline --decorate";
+        };
     };
 }
