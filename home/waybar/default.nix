9fbad3de19cfdecf1d4f24fcccf3b43032eaec7d
LalieA <83229713+LalieA@users.noreply.github.com>
2023-11-02 22:04:27 +0100

    waybar/swaync: Fix swaync showing at every click on waybar
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -150,6 +150,7 @@ in
             };
 
             "custom/notifications" = {
+                layer = "bottom";
                 tooltip = false;
                 format = "{} {icon}";
                 format-icons = {
