9fdd500cb809a0d82f9f4925f49f363074b5be72
LalieA <83229713+LalieA@users.noreply.github.com>
2024-02-17 01:38:38 +0100

    live: Add security hardening rules
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- modules/live.nix
+++ modules/live.nix
@@ -33,6 +33,60 @@
 
     powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
+    ## SECURITY HARDENING
+    ## Inspired from nixpkgs/nixos/modules/profiles/hardened.nix
+    boot.kernelParams = [
+        # Don't merge slabs
+        "slab_nomerge"
+
+        # Overwrite free'd pages
+        "page_poison=1"
+
+        # Enable page allocator randomization
+        "page_alloc.shuffle=1"
+
+        # Disable debugfs
+        "debugfs=off"
+    ];
+
+    boot.kernel.sysctl = {
+        # Disable BPF JIT
+        "net.core.bpf_jit_enable" = false;
+
+        # Disable ftrace debugging
+        "kernel.ftrace_enabled" = false;
+
+        # Enable strict reverse path filtering (that is, do not attempt to route
+        # packets that "obviously" do not belong to the iface's network; dropped
+        # packets are logged as martians).
+        "net.ipv4.conf.all.log_martians" = true;
+        "net.ipv4.conf.all.rp_filter" = "1";
+        "net.ipv4.conf.default.log_martians" = true;
+        "net.ipv4.conf.default.rp_filter" = "1";
+
+        # Ignore broadcast ICMP (mitigate SMURF)
+        "net.ipv4.icmp_echo_ignore_broadcasts" = true;
+
+        # Ignore incoming ICMP redirects (note: default is needed to ensure that the
+        # setting is applied to interfaces added after the sysctls are set)
+        "net.ipv4.conf.all.accept_redirects" = false;
+        "net.ipv4.conf.all.secure_redirects" = false;
+        "net.ipv4.conf.default.accept_redirects" = false;
+        "net.ipv4.conf.default.secure_redirects" = false;
+        "net.ipv6.conf.all.accept_redirects" = false;
+        "net.ipv6.conf.default.accept_redirects" = false;
+
+        # Ignore outgoing ICMP redirects (this is ipv4 only)
+        "net.ipv4.conf.all.send_redirects" = false;
+        "net.ipv4.conf.default.send_redirects" = false;
+    };
+
+    security = {
+        protectKernelImage = true;
+        forcePageTableIsolation = true;
+    };
+
+
     ## SYSTEM CONFIGURATION
     # Time zone
     time.timeZone = "Etc/UTC";
