1f6382b29187c53b976cca5427770d4cd9f08f88
LalieA <83229713+LalieA@users.noreply.github.com>
2025-01-05 14:16:44 +0100

    misc: Update Ankama Launcher hash
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- modules/misc/ankama-launcher.nix
+++ modules/misc/ankama-launcher.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }:
 
-with pkgs;
 let
   name = "ankama-launcher";
   src = pkgs.fetchurl {
     url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-    sha256 = "1z0kw788hlgz6jfc660d5ldqd6sa9dfcpp68krqzl8qqnhba7h5j"; # Change for the sha256 you get after running nix-prefetch-url https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage
+    sha256 = "06yri31m2k47xhbhalz16mbigmpxrl8dy3k0si8l9mkdyhpa0wpj"; # Change for the sha256 you get after running nix-prefetch-url https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage
  };
- appimageContents = appimageTools.extractType2 { inherit name src; };
+ appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
 
 in
 
@@ -19,4 +18,8 @@ pkgs.appimageTools.wrapType2 {
     sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
     install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
   '';
+
+  extraPkgs = pkgs: [
+      pkgs.wine-staging
+  ];
 }
