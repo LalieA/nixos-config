3c0dd9149c47a0f1063c5b5c2c095a8b99774248
LalieA <83229713+LalieA@users.noreply.github.com>
2025-01-05 14:16:00 +0100

    programs: Add SweetHome3D
    Signed-off-by: LalieA <83229713+LalieA@users.noreply.github.com>

--- home/programs/packages/misc.nix
+++ home/programs/packages/misc.nix
@@ -4,6 +4,11 @@ let
         # productivity
         obsidian
         zotero
+
+        # 3d
+        sweethome3d.application
+        sweethome3d.textures-editor
+        sweethome3d.furniture-editor
     ];
 in {
     home.packages = miscPackages;
